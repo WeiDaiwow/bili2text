b6c12dc600e3d99447c1f5c3bd2d09730ef24390
lanbinleo <lanbinwolf@gmail.com>
2025-04-03 11:28:04 +0800

    feat: 添加Flask依赖并更新主程序以启动Web服务器，注释掉自检功能

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from src.selfcheck import SelfCheck
+from src.config import SERVER_HOST, SERVER_PORT, DEBUG_MODE
+from src.webserver.server import app
 
 if __name__ == "__main__":
-    SelfCheck().run()
+    # SelfCheck().run()
+    app.run(
+        host=SERVER_HOST,
+        port=SERVER_PORT,
+        debug=DEBUG_MODE
+    )
